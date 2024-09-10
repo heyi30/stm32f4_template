85948273db9377c5107b996b6d75bda2c08d967a
Specific-Cola <1528284020@qq.com>
2024-09-10 15:17:53 +0800

    fix: 修复调试无法runtoentypoint main的问题

--- user/app/AttitudeTask.cc
+++ user/app/AttitudeTask.cc
@@ -13,7 +13,7 @@ using irobot_ec::modules::algorithm::MahonyAhrs;
 // 注意！！：不要在全局作用域中创建传感器对象，因为这样会导致对象的构造函数在main函数之前调用，而HAL库此时还未初始化
 // 如果需要全局访问，可以将对象指针声明为全局变量，然后在任务函数中new一个对象绑定到指针上
 // 像这样：
-// BMI088 *bmi088;
+BMI088 *bmi088;
 // void task() {
 //   bmi088 = new BMI088(...);
 // }
@@ -21,7 +21,7 @@ using irobot_ec::modules::algorithm::MahonyAhrs;
 void AttitudeTask(void const * argument) {
   // 对于BMI088，这个传感器由两个SPI设备组成，一个是加速度计，一个是陀螺仪
   // 使用时分别传入SPI总线指针、加速度计的CS引脚和陀螺仪的CS引脚
-  BMI088 bmi088(hspi1, CS1_ACCEL_GPIO_Port, CS1_ACCEL_Pin, CS1_GYRO_GPIO_Port,
+  bmi088 = new BMI088(hspi1, CS1_ACCEL_GPIO_Port, CS1_ACCEL_Pin, CS1_GYRO_GPIO_Port,
                 CS1_GYRO_Pin);
   // IST8310是一个I2C设备，传入I2C总线指针和RESET引脚
 //   IST8310 ist8310(hi2c3, GPIOG, GPIO_PIN_6);
@@ -32,7 +32,7 @@ void AttitudeTask(void const * argument) {
     osDelay(1);
 
     // 更新传感器数据
-    bmi088.Update();
+    bmi088->Update();
     // ist8310.Update();
 
     // // 更新姿态解算器
@@ -40,8 +40,8 @@ void AttitudeTask(void const * argument) {
     //               bmi088.accel_x(), bmi088.accel_y(), bmi088.accel_z(),
     //               ist8310.mag_x(), ist8310.mag_y(), ist8310.mag_z());
     // 更新姿态解算器(禁用磁力计)
-    mahony.UpdateImu(bmi088.gyro_x(), bmi088.gyro_y(), bmi088.gyro_z(),
-                     bmi088.accel_x(), bmi088.accel_y(), bmi088.accel_z());
+    mahony.UpdateImu(bmi088->gyro_x(), bmi088->gyro_y(), bmi088->gyro_z(),
+                     bmi088->accel_x(), bmi088->accel_y(), bmi088->accel_z());
 
     // 获取姿态数据(欧拉角，弧度)
     mahony.euler_pitch();

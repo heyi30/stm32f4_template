12116d7ddac134ee6b6f91d3923ece6f82f128d1
lunarifish <messenger@lunariio.work>
2024-09-13 13:18:25 +0800

    fix: fixed a lot of shit

--- src/app/AttitudeTask.cc
+++ src/app/AttitudeTask.cc
@@ -1,13 +1,11 @@
 #include "cmsis_os.h"
-// #include "i2c.h"
 #include "spi.h"
 #include "AttitudeTask.h"
 
-#include "irobotEC.hpp"
-#include "irobotEC/modules/algorithm/mahony_ahrs.h"
+#include "irobotec.hpp"
+#include "irobotec/modules/algorithm/mahony_ahrs.h"
 
 using irobot_ec::device::BMI088;
-// using irobot_ec::device::IST8310;
 using irobot_ec::modules::algorithm::MahonyAhrs;
 
 // 注意！！：不要在全局作用域中创建传感器对象，因为这样会导致对象的构造函数在main函数之前调用，而HAL库此时还未初始化
@@ -18,13 +16,12 @@ BMI088 *bmi088;
 //   bmi088 = new BMI088(...);
 // }
 
-void AttitudeTask(void const * argument) {
+void AttitudeTask(void const *unused) {
   // 对于BMI088，这个传感器由两个SPI设备组成，一个是加速度计，一个是陀螺仪
   // 使用时分别传入SPI总线指针、加速度计的CS引脚和陀螺仪的CS引脚
-  bmi088 = new BMI088(hspi1, CS1_ACCEL_GPIO_Port, CS1_ACCEL_Pin, CS1_GYRO_GPIO_Port,
-                CS1_GYRO_Pin);
+  bmi088 = new BMI088(hspi1, CS1_ACCEL_GPIO_Port, CS1_ACCEL_Pin, CS1_GYRO_GPIO_Port, CS1_GYRO_Pin);
   // IST8310是一个I2C设备，传入I2C总线指针和RESET引脚
-//   IST8310 ist8310(hi2c3, GPIOG, GPIO_PIN_6);
+  //   IST8310 ist8310(hi2c3, GPIOG, GPIO_PIN_6);
   // 创建Mahony姿态解算器对象，传入采样率
   MahonyAhrs mahony(1000.0f);
 
@@ -40,8 +37,8 @@ void AttitudeTask(void const * argument) {
     //               bmi088.accel_x(), bmi088.accel_y(), bmi088.accel_z(),
     //               ist8310.mag_x(), ist8310.mag_y(), ist8310.mag_z());
     // 更新姿态解算器(禁用磁力计)
-    mahony.UpdateImu(bmi088->gyro_x(), bmi088->gyro_y(), bmi088->gyro_z(),
-                     bmi088->accel_x(), bmi088->accel_y(), bmi088->accel_z());
+    mahony.UpdateImu(bmi088->gyro_x(), bmi088->gyro_y(), bmi088->gyro_z(), bmi088->accel_x(), bmi088->accel_y(),
+                     bmi088->accel_z());
 
     // 获取姿态数据(欧拉角，弧度)
     mahony.euler_pitch();
